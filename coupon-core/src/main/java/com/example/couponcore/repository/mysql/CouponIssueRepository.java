85f2bca7c54c978e3177453ad9f3e7f66a920fe3
ajeng518 <elegant518@gmail.com>
2026-08-28 23:23:03 +0900

    ì¿쿠폰발급 api qcouponIssue 수정

--- coupon-core/src/main/java/com/example/couponcore/repository/mysql/CouponIssueRepository.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/mysql/CouponIssueRepository.java
@@ -6,11 +6,16 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-//import static com.example.couponcore.model.QCouponIssue.couponIssue;
+import static com.example.couponcore.model.QCouponIssue.couponIssue;
 
 @RequiredArgsConstructor
 @Repository
 public class CouponIssueRepository {
     private final JPAQueryFactory queryFactory;
-
+    public CouponIssue findFirstCouponIssue(long couponId, long userId) {
+        return queryFactory.selectFrom(couponIssue)
+                .where(couponIssue.couponId.eq(couponId))
+                .where(couponIssue.userId.eq(userId))
+                .fetchFirst();
+    }
 }
