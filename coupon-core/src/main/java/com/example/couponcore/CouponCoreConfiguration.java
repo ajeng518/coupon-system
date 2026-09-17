eaaedf55ad66a5e86c035367a71604facd2b35eb
ajeng518 <elegant518@gmail.com>
2026-09-17 23:17:26 +0900

    ãCh06. 쿠폰 발급 요청 처리 기능 개선 - 쿠폰 캐시 적용

--- coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
@@ -3,7 +3,9 @@ package com.example.couponcore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @EnableJpaAuditing
 @ComponentScan
 @EnableAutoConfiguration
